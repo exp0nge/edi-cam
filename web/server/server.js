6065eee97d93dcf8dc8d035c8ed09aa24f7b7b07
exp0nge <mdislamwork@gmail.com>
2017-04-13 19:42:02 -0400

    add gpio and functions

--- web/server/server.js
+++ web/server/server.js
@@ -1,9 +1,10 @@
 // modules
 var childProcess = require('child_process')
-  , express = require('express')
-  , http = require('http')
-  , morgan = require('morgan')
-  , ws = require('ws');
+    , express = require('express')
+    , http = require('http')
+    , morgan = require('morgan')
+    , ws = require('ws')
+    , mraa = require('mraa');
 
 // configuration files
 var configServer = require('./lib/config/server');
@@ -19,7 +20,7 @@ require('./lib/routes').serveIndex(app, configServer.staticFolder);
 
 // HTTP server
 http.createServer(app).listen(app.get('port'), function () {
-  console.log('HTTP server listening on port ' + app.get('port'));
+    console.log('HTTP server listening on port ' + app.get('port'));
 });
 
 /// Video streaming section
@@ -30,52 +31,106 @@ var width = 320;
 var height = 240;
 
 // WebSocket server
-var wsServer = new (ws.Server)({ port: configServer.wsPort });
+var wsServer = new (ws.Server)({port: configServer.wsPort});
 console.log('WebSocket server listening on port ' + configServer.wsPort);
 
-wsServer.on('connection', function(socket) {
-  // Send magic bytes and video size to the newly connected socket
-  // struct { char magic[4]; unsigned short width, height;}
-  var streamHeader = new Buffer(8);
+wsServer.on('connection', function (socket) {
+    // Send magic bytes and video size to the newly connected socket
+    // struct { char magic[4]; unsigned short width, height;}
+    var streamHeader = new Buffer(8);
 
-  streamHeader.write(STREAM_MAGIC_BYTES);
-  streamHeader.writeUInt16BE(width, 4);
-  streamHeader.writeUInt16BE(height, 6);
-  socket.send(streamHeader, { binary: true });
+    streamHeader.write(STREAM_MAGIC_BYTES);
+    streamHeader.writeUInt16BE(width, 4);
+    streamHeader.writeUInt16BE(height, 6);
+    socket.send(streamHeader, {binary: true});
 
-  console.log('New WebSocket Connection (' + wsServer.clients.length + ' total)');
+    console.log('New WebSocket Connection (' + wsServer.clients.length + ' total)');
 
-  socket.on('close', function(code, message){
-    console.log('Disconnected WebSocket (' + wsServer.clients.length + ' total)');
-  });
+    socket.on('close', function (code, message) {
+        console.log('Disconnected WebSocket (' + wsServer.clients.length + ' total)');
+    });
+
 });
 
-wsServer.broadcast = function(data, opts) {
-  for(var i in this.clients) {
-    if(this.clients[i].readyState == 1) {
-      this.clients[i].send(data, opts);
+wsServer.on('message', function incoming(data, flags) {
+    console.log(data);
+    console.log(flags);
+});
+
+wsServer.broadcast = function (data, opts) {
+    for (var i in this.clients) {
+        if (this.clients[i].readyState == 1) {
+            this.clients[i].send(data, opts);
+        }
+        else {
+            console.log('Error: Client (' + i + ') not connected.');
+        }
     }
-    else {
-      console.log('Error: Client (' + i + ') not connected.');
-    }
-  }
 };
 
 // HTTP server to accept incoming MPEG1 stream
 http.createServer(function (req, res) {
-  console.log(
-    'Stream Connected: ' + req.socket.remoteAddress +
-    ':' + req.socket.remotePort + ' size: ' + width + 'x' + height
-  );
+    console.log(
+        'Stream Connected: ' + req.socket.remoteAddress +
+        ':' + req.socket.remotePort + ' size: ' + width + 'x' + height
+    );
 
-  req.on('data', function (data) {
-    wsServer.broadcast(data, { binary: true });
-  });
+    req.on('data', function (data) {
+        wsServer.broadcast(data, {binary: true});
+    });
 }).listen(configServer.streamPort, function () {
-  console.log('Listening for video stream on port ' + configServer.streamPort);
+    console.log('Listening for video stream on port ' + configServer.streamPort);
 
-  // Run do_ffmpeg.sh from node                                                   
-  childProcess.exec('../../bin/do_ffmpeg.sh');
+    // Run do_ffmpeg.sh from node
+    childProcess.exec('../../bin/do_ffmpeg.sh');
 });
+
+// Pin initialization
+var leftBackward = mraa.Gpio(2),
+    leftForward = mraa.Gpio(3),
+    rightForward = mraa.Gpio(4),
+    rightBackward = mraa.Gpio(5);
+leftBackward.dir(mraa.DIR_OUT);
+leftForward.dir(mraa.DIR_OUT);
+rightForward.dir(mraa.DIR_OUT);
+rightBackward.dir(mraa.DIR_OUT);
+
+var DEFAULT_DURATION = 0.3;
+
+function roverForward(secs) {
+    leftForward.write(1);
+    rightForward.write(1);
+    setTimeout(function () {
+        leftForward.write(0);
+        rightForward.write(0);
+    }, secs || DEFAULT_DURATION);
+}
+
+function roverBackward(secs) {
+    leftBackward.write(1);
+    rightBackward.write(1);
+    setTimeout(function () {
+        leftBackward.write(0);
+        rightBackward.write(0);
+    }, secs || DEFAULT_DURATION);
+}
+
+function roverLeft(secs) {
+    leftBackward.write(1);
+    rightForward.write(1);
+    setTimeout(function () {
+        leftBackward.write(0);
+        rightForward.write(0);
+    }, secs || DEFAULT_DURATION);
+}
+
+function roverRight(secs) {
+    leftForward.write(1);
+    rightBackward.write(1);
+    setTimeout(function () {
+        leftForward.write(0);
+        rightBackward.write(0);
+    }, secs || DEFAULT_DURATION);
+}
 
 module.exports.app = app;
